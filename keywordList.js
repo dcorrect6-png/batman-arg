4cd71256ba5a6501e8f7538893abc8be148bec73
LK411 <dcorrect6@gmail.com>
2026-07-09 11:11:00 +0800

    Create keywordList.js

---
+++ keywordList.js
@@ -0,0 +1,16 @@
+// keywordList.js 线索检索关键词库
+const keywordJump = {
+    "蝙蝠侠": "batman.html",
+    "换人": "change.html",
+    "帖子": "post.html",
+    "汉堡": "msg1.html",
+    "汉堡摊": "msg1.html",
+    "套餐": "msg1.html",
+    "绿箭侠": "msg2.html",
+    "丧钟": "msg2.html",
+    "照片": "msg2.html",
+    "UHGURELQ": "msg4.html",
+    "谜题": "msg4.html",
+    "邮箱": "msg4.html",
+    "隐私": "msg4.html"
+};
